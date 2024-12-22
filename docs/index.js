fd40af217fc996b72e5e842a650fb52395f819ed
truthixify <truthixify@gmail.com>
2024-12-22 12:40:18 +0100

    fixed game storage link

--- docs/index.js
+++ docs/index.js
@@ -183,7 +183,7 @@ async function run() {
     dropdownItems.forEach((item, i) => {
         item.addEventListener("click", () => {
             // Fetch the file content
-            fetch(games[i].src)
+            fetch("https://github.com/Truthixify/chip8/tree/main/docs" + games[i].src)
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`Error fetching file: ${response.statusText}`)
